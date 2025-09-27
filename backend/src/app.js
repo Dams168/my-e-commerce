fbfdb81507418250ce05c0f4fa5aba47a52ec488
Dams168 <adamrohman2003@gmail.com>
2025-09-27 23:20:47 +0700

    add: documentation api with swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import { errorMiddleware } from './middleware/error-middleware.js';
 import { publicRouter } from './router/public-api.js';
 import { apiRouter } from './router/api.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './docs/swagger.json' with { type: 'json' };
 
 const app = express();
 const PORT = 3000;
@@ -12,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRouter)
 app.use('/api', publicRouter)
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(errorMiddleware);
 app.listen(PORT, () => {
