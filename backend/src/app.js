9a8a59355bcd5998a2d51286ed334a39f865b67e
Dams168 <adamrohman2003@gmail.com>
2025-09-29 22:51:19 +0700

    add: cors permission for dev

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,14 @@ import { publicRouter } from './router/public-api.js';
 import { apiRouter } from './router/api.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './docs/swagger.json' with { type: 'json' };
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use('/api', apiRouter)
 app.use('/api', publicRouter)
